8a8cb1850c869537b134488d395c287b342fd7db
Will Long <long@latech.edu>
2021-12-06 17:13:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from distutils.core import setup
 setup(
   name = 'TEEPy',
   packages = ['TEEPy'],
-  version = '0.3',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Tech Engineering Exam in Python',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  version = '0.3',
+  license='MIT',
+  description = 'Tech Engineering Exam in Python',
+  author = 'William Long',
+  author_email = 'admin@longapalooza.com',
+  url = 'https://github.com/longapalooza/TEEPy',
+  download_url = 'https://github.com/longapalooza/TEEPy/archive/refs/tags/v_03.tar.gz',
+  keywords = ['Tech', 'Exam', 'Engineering'],
+  install_requires = [            # I get to this in a second
+          'beautifulsoup',
+          'cefpython3',
+          'pint',
+          'pyppeteer',
+          'xlsxwriter',
       ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
   ],
 )
