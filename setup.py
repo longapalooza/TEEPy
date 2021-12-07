4bf3a973cd585bdfcf6b2e9b496425f37e0d9209
Will Long <long@latech.edu>
2021-12-06 21:51:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'TEEPy',
-  packages = ['TEEPy'],
-  version = '0.3.3',
+  name = 'teepy',
+  packages = ['teepy'],
+  version = '0.3',
   license = 'MIT',
   description = 'Tech Engineering Exam in Python',
   author = 'William Long',
   author_email = 'admin@longapalooza.com',
-  url = 'https://github.com/longapalooza/TEEPy',
-  download_url = 'https://github.com/longapalooza/TEEPy/archive/refs/tags/v0.3.3-alpha.tar.gz',
+  url = 'https://github.com/longapalooza/teepy',
+  download_url = 'https://github.com/longapalooza/teepy/archive/refs/tags/v0.3-beta.tar.gz',
   keywords = ['Tech', 'Exam', 'Engineering'],
   install_requires = [
           'beautifulsoup4',
@@ -18,11 +18,13 @@ setup(
           'xlsxwriter',
       ],
   classifiers = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
   ],
-  include_package_data = True,
+  package_data = {
+    "": ['teepy.html', 'teepy.css'],    
+  },
 )
