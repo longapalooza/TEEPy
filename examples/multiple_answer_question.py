db1519e3f81482db2cbc906983e51e4bdbfb39ac
Will Long <long@latech.edu>
2021-12-16 16:41:04 -0600

    Update multiple_answer_question.py

--- examples/multiple_answer_question.py
+++ examples/multiple_answer_question.py
@@ -1,4 +1,7 @@
 import random
+import sys
+lib_dir = '../teepy/'
+sys.path.insert(0, lib_dir)
 import teepy
 
 ind = 0
@@ -7,13 +10,13 @@ def PROBLEM(ind):
     '''How many licks does it take to get to the center of a Tootsie
 Pop?'''
 
-    answer = ['3', 'The world may never know.']
+    answer = [3, 'The world may never know.']
 
     return {'answer': answer}
 
 def CHOICES(ind):
     
-    choices = ['1', '2']
+    choices = [1, 2]
     
     choices.extend(teepy.get_answers(PROBLEM(ind)))
     
